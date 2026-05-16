738074458142ad50b35c64505caa9978b5186619
naylazaky <naylaazakyfauziah@gmail.com>
2026-05-16 17:12:47 +0800

    feat: ubah package name ke com.nomnom.recipes dan tambah dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nomnom"
+    namespace = "com.nomnom.recipes"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.nomnom"
+        applicationId = "com.nomnom.recipes"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -43,4 +43,30 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.7.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/test/java/com/nomnom/recipes/ExampleUnitTest.java
+++ app/src/test/java/com/nomnom/recipes/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.nomnom;
+package com.recipes.nomnom;
 
 import org.junit.Test;
 
